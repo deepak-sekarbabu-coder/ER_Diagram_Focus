10b6c7e49f2104386382e7094ee3dc4a274b915c
Deepak Sekarbabu <princedeepak2012@gmail.com>
2025-07-23 19:29:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ER_Diagram_Focus/',
+  base: '/er-diagram-focus/',
   server: {
     host: "::",
     port: 8080,
